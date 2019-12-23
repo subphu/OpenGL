3dd161452824b30ee0f521a90c9264dd81769bc6
Subroto <subrotoph@gmail.com>
2019-12-23 19:58:18 +0900

    Add setUniformMatrix in shader

--- Sandbox/shader.h
+++ Sandbox/shader.h
@@ -9,6 +9,7 @@
 #define FRAGMENT "FRAGMENT"
 
 #include <GL/glew.h>
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -72,6 +73,14 @@ public:
     
     void setUniform4f(const std::string &name, float v0,float v1, float v2, float v3) const {
         glUniform4f(glGetUniformLocation(ID, name.c_str()), v0, v1, v2, v3);
+    }
+    
+    void setUniformMatrix3fv(const std::string &name, const glm::mat3 &mat) const {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+    
+    void setUniformMatrix4fv(const std::string &name, const glm::mat4 &mat) const {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
     
     GLint getAttribLocation(const std::string &name) {
