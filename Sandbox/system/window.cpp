b9f5b23b1053828d7a67dfbb64c8362489f92766
Subroto <subrotoph@gmail.com>
2020-02-06 18:13:49 +0900

    Change opengl version

--- Sandbox/system/window.cpp
+++ Sandbox/system/window.cpp
@@ -20,8 +20,8 @@ void System::initGlLibrary() {
 void System::settingWindow() {
     glfwInit();
     
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
     
